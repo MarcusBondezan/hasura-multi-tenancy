b25391bc1e916ee8ee15381029956c958097a3d5
Marcus Bondezan <marcus.bondezan@gmail.com>
2021-06-23 08:18:32 -0300

    identing migration

--- hasura/migrations/default/1623696739041_create_table_public_user/up.sql
+++ hasura/migrations/default/1623696739041_create_table_public_user/up.sql
@@ -1,1 +1,7 @@
-CREATE TABLE "public"."user" ("id" serial NOT NULL, "username" text NOT NULL, "created_at" timestamptz NOT NULL DEFAULT now(), PRIMARY KEY ("id") , UNIQUE ("username"));
+CREATE TABLE "public"."user" (
+  "id" serial NOT NULL, 
+  "username" text NOT NULL, 
+  "created_at" timestamptz NOT NULL DEFAULT now(), 
+  PRIMARY KEY ("id") , 
+  UNIQUE ("username")
+);
